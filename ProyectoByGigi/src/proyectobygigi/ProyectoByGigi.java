b240517bb532cf8878b0e01787093ee0ca38c3a5
georginaakel <georgina.akel@correo.unimet.edu.ve>
2023-02-15 21:29:06 -0400

    Create User Class

--- ProyectoByGigi/src/proyectobygigi/ProyectoByGigi.java
+++ ProyectoByGigi/src/proyectobygigi/ProyectoByGigi.java
@@ -14,7 +14,9 @@ public class ProyectoByGigi {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Usuario user = new Usuario ("Gigi");
+        user.mostrarNombre();
+        user.Mostrar_Nombre2();
     }
     
 }
